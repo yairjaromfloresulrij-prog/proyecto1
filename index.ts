5845ff6735bfb9f0f4e60cafe749d3c2a662541b
Yair Flores <yairjaromfloresulrij@gmail.com>
2026-08-10 21:35:08 -0300

    feat: setup inicial y variables globales

---
+++ index.ts
@@ -0,0 +1,15 @@
+
+// 🚫 No eliminar las líneas de arriba ⬆️
+
+// ✍️ Escribe tu código aquí 👇
+
+let systemName: string = "CS";
+let version: number = 1.6;
+let userName: string = "Kan";
+
+console.log("==================================");
+console.log("   " + systemName + " v " + version );
+console.log(" ¡Bienvenido, " + userName + "!");
+console.log("==================================");
+
+// 🚫 No eliminar las líneas de abajo ⬇️
